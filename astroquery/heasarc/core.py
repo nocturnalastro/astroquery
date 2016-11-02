1c4350937feb5cf9721cf37a78495bae46ec2d4b
nocturnalastro <thenocturnalastrostudent@gmail.com>
2016-11-02 12:43:50 +0000

    added some more parameters and refactored a bit to remove duplication

--- astroquery/heasarc/core.py
+++ astroquery/heasarc/core.py
@@ -10,6 +10,9 @@ from . import conf
 
 __all__ = ['Heasarc', 'HeasarcClass']
 
+allowed_rad_units = ["degrees", "arcmin", "arcsec"]
+common _parameter_description = "{PUT DESCIPTION HERE   }"
+
 
 @async_to_sync
 class HeasarcClass(BaseQuery):
@@ -20,15 +23,21 @@ class HeasarcClass(BaseQuery):
     URL = conf.server
     TIMEOUT = conf.timeout
 
-    def query_object_async(self, object_name, table, cache=True,
-                           get_query_payload=False,
-                           display_mode='FitsDisplay'):
-        """TODO: document this!
+    def _base_query(self, table, object_name="", search_radius="Default", radius_unit="arcmin",
+                    result_max=1000, display_mode='FitsDisplay',
+                    get_query_payload=False, cache=True, **kwargs):
+        """This is a base for the all querys to avoid duplation of
+         code specific parameters are passed as keywords"""
 
-        (maybe start by copying over from some other service.)
-        """
-        request_payload = dict()
-        request_payload['Entry'] = object_name
+        radius_unit = radius_unit.lower()
+        if radius_unit in allowed_rad_units:
+            raise ValueError("radius_unit is not {allowed}".format(", ".join(allowed_rad_units)))
+
+        request_payload = kwargs
+        request_payload["Entry"] = object_name
+        request_payload["Radius"] = search_radius
+        request_payload["Radius_unit"] = radius_unit
+        request_payload["ResultMax"] = result_max
         request_payload['tablehead'] = ('BATCHRETRIEVALCATALOG_2.0 {}'
                                         .format(table))
         request_payload['Action'] = 'Query'
@@ -40,6 +49,33 @@ class HeasarcClass(BaseQuery):
         response = self._request('GET', self.URL, params=request_payload,
                                  timeout=self.TIMEOUT, cache=cache)
         return response
+
+    def query_object_async(self, object_name, table, search_radius="Default",
+                           radius_unit="arcmin", result_max=1000, cache=True,
+                           get_query_payload=False, display_mode='FitsDisplay'):
+        """TODO: document this!
+        {common}
+        """.format(common=common _parameter_description)
+        return self._base_query(table, object_name=object_name, search_radius=search_radius, radius_unit=radius_unit,
+                                result_max=result_max, display_mode=display_mode,
+                                get_query_payload=get_query_payload, cache=cache)
+
+    def query_id_async(self, obsid, table, op="=", cache=True,
+                       get_query_payload=False,
+                       display_mode='FitsDisplay'):
+        """TODO: document this!
+        {common}
+        """.format(common=common _parameter_description)
+
+        request_payload = dict()
+        request_payload['Entry'] = ""
+        request_payload['tablehead'] = 'name=heasarc_{table}'.format(table=table)
+        request_payload['bparam_obsid'] = "{op}{obsid}".format(op=op, obsid=obsid)
+
+        return self._base_query(table, search_radius=search_radius, radius_unit=radius_unit,
+                                result_max=result_max, display_mode=display_mode,
+                                get_query_payload=get_query_payload, cache=cache,
+                                **request_payload)
 
     def _fallback(self, content):
         """
@@ -69,26 +105,6 @@ class HeasarcClass(BaseQuery):
             new_table.append("".join(newline))
         data = BytesIO(bytes(content.replace(old_table, "\n".join(new_table)), encoding="UTF-8"))
         return Table.read(data, hdu=1)
-
-    def query_id_async(self, obsid, table, op="=", cache=True,
-                       get_query_payload=False,
-                       display_mode='FitsDisplay'):
-        """TODO: document this!
-        (maybe start by copying over from some other service.)
-        """
-        request_payload = dict()
-        request_payload['Entry'] = ""
-        request_payload['tablehead'] = 'name=heasarc_{table}'.format(table=table)
-        request_payload['bparam_obsid'] = "{op}{obsid}".format(op=op, obsid=obsid)
-        request_payload['Action'] = 'Query'
-        request_payload['displaymode'] = display_mode
-
-        if get_query_payload:
-            return request_payload
-
-        response = self._request('GET', self.URL, params=request_payload,
-                                 timeout=self.TIMEOUT, cache=cache)
-        return response
 
     def _parse_result(self, response, verbose=False):
         # if verbose is False then suppress any VOTable related warnings
